254b431b9dd8959292a5e7ada275fa50de6b4980
luminmei <514911632@qq.com>
2019-02-18 18:03:04 +0800

    修改分隔符

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ var globalConfig = {};
 
 var conf = fs.readFileSync("./server.conf");
 
-var configArr = conf.toString().split("\r\n");
+var configArr = conf.toString().split("\n");
 
 for (var i = 0; i < configArr.length; i ++) {
     globalConfig[configArr[i].split("=")[0].trim()] = configArr[i].split("=")[1].trim()
